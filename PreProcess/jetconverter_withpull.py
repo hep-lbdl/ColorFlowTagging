rotation fixes, signal choice streamlined

PreProcess/jetconverter_withpull.py
@@ -18,7 +18,8 @@ import logging
 
 import numpy as np
 
-from jettools import plot_mean_jet, buffer_to_jet, is_signal
+from jettools import plot_mean_jet
+from processing import buffer_to_jet, is_signal
 import array
 
 
@@ -175,7 +176,7 @@ if __name__ == '__main__':
                                 jet_nb, n_entries, fname
                             )
                         )
-                    if (np.abs(jet['LeadingEta']) < 2) & (jet['LeadingPt'] > float(args.ptmin)) & (jet['LeadingPt'] < float(args.ptmax)) & (jet['LeadingM'] < float(95)) & (jet['LeadingM'] > float(65)):
+                    if (np.abs(jet['LeadingEta']) < 2) & (jet['LeadingPt'] > float(300)) & (jet['LeadingPt'] < float(600)) & (jet['LeadingM'] < float(150)) & (jet['LeadingM'] > float(100)):
 
                         buf = buffer_to_jet(jet, tag, max_entry=100000, pix=pix_per_side)
                         if args.dump:
@@ -215,7 +216,7 @@ if __name__ == '__main__':
             logger.info('Skipping file {}'.format(fname))
         except AttributeError:
             logger.info('Skipping file {} for compatibility reasons'.format(fname))
-    if args.dump:
+    if args.dump and tree != None:
         tree.write()
         ROOTfile.close()
     

jettools/processing.py
@@ -7,7 +7,7 @@ Simple utilities for processing the junk that comes out of the ntuple event gene
 
 import numpy.linalg as la
 import numpy as np
-from .jettools import rotate_jet, flip_jet, plot_mean_jet
+from jettools import rotate_jet, flip_jet, plot_mean_jet
 from ROOT import*
 
 def sum_image(image):
@@ -119,7 +119,7 @@ def buffer_to_jet(entry, tag = 0, side = 'r', max_entry = None, pix = 25):
     else:
         e, p = (entry['SubLeadingEta'], entry['SubLeadingPhi'])
     
-    angle = np.arctan(p / e) + 2.0 * np.arctan(1.0)
+    angle = np.arctan2(p, e) + 2.0 * np.arctan(1.0)
 
     if (-np.sin(angle) * e + np.cos(angle) * p) > 0:
         angle += -4.0 * np.arctan(1.0)
@@ -146,7 +146,7 @@ def buffer_to_jet(entry, tag = 0, side = 'r', max_entry = None, pix = 25):
     exit(1)
     '''
 
-    image = flip_jet(rotate_jet(np.array(entry['Intensity']), -4*np.arctan(1.0), normalizer=4000.0, dim=pix), side)
+    image = flip_jet(rotate_jet(np.array(entry['Intensity']), -angle, normalizer=4000.0, dim=pix), side) # change between (-angle <-> -4*np.arctan(1.0)) if testing
     e_norm = np.linalg.norm(image)
     #e_norm = 1./4000.0
     return ((image / e_norm).astype('float32'), np.float32(tag), 